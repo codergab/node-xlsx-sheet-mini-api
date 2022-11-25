f4e20b2864144e6f97425d96a6bdac85dcef3020
Gabriel Adewumi <codergab@gmail.com>
2022-11-25 14:15:46 +0100

    Forgot cors?

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const { GoogleSpreadsheet } = require('google-spreadsheet')
 const helment = require('helmet')
+const cors = require('cors')
 require('dotenv-safe').config()
 
 const app = express()
 app.use(express.json())
-
+app.use(cors())
 app.use(helment())
 
 const creds = require('./app-creds.json');
